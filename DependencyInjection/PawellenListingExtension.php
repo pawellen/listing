4bcc7cdd3c0f550cd1bb1e92254490c67dec6f9a
pawel.len <pawel.len@gmail.com>
2020-03-15 01:26:35 +0100

    working initial - metadata files

--- DependencyInjection/PawellenListingExtension.php
+++ DependencyInjection/PawellenListingExtension.php
@@ -2,8 +2,10 @@
 
 namespace Pawellen\ListingBundle\DependencyInjection;
 
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 
 class PawellenListingExtension extends Extension
@@ -11,8 +13,13 @@ class PawellenListingExtension extends Extension
     public function load(array $configs, ContainerBuilder $container)
     {
         $configuration = new Configuration();
-
         $config = $this->processConfiguration($configuration, $configs);
         $container->setParameter('pawellen_listing', $config);
+
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../Resources/config')
+        );
+        $loader->load('services.yaml');
     }
 }
